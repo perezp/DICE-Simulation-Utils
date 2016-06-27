922c7be1e687aa929e834e0495e256d7486df6bd
Abel Gómez <abel.gomez@unizar.es>
2016-06-27 16:23:30 +0200

    Remove LIP6 to dot converter and use Acceleo

--- es.unizar.disco.pnml.utils/src/es/unizar/disco/pnml/utils/handlers/ConvertToDot.java
+++ es.unizar.disco.pnml.utils/src/es/unizar/disco/pnml/utils/handlers/ConvertToDot.java
@@ -1,36 +1,31 @@
 package es.unizar.disco.pnml.utils.handlers;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
 
+import org.eclipse.acceleo.common.preference.AcceleoPreferences;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import es.unizar.disco.pnml.utils.PnmlUtilsPlugin;
-import fr.lip6.move.pnml.framework.hlapi.HLAPIRootClass;
-import fr.lip6.move.pnml.framework.utils.ModelRepository;
-import fr.lip6.move.pnml.framework.utils.PNMLUtils;
-import fr.lip6.move.pnml.framework.utils.exception.ImportException;
-import fr.lip6.move.pnml.framework.utils.exception.InvalidIDException;
-import fr.lip6.move.pnml.framework.utils.exception.VoidRepositoryException;
-import fr.lip6.move.pnml.todot.processors.MainProcessor;
-import fr.lip6.move.pnml.todot.processors.Processor;
-import fr.lip6.move.pnml.todot.utils.UnHandledType;
+import es.unizar.disco.pnml.utils.pnml2dot.Pnml2dot;
 
 public class ConvertToDot extends AbstractHandler {
 
@@ -44,35 +39,36 @@ public class ConvertToDot extends AbstractHandler {
 			Job job = new Job(ID) {
 				@Override
 				protected IStatus run(IProgressMonitor monitor) {
-					if (monitor == null) monitor = new NullProgressMonitor();
+					if (monitor == null)
+						monitor = new NullProgressMonitor();
+					MultiStatus status = new MultiStatus(PnmlUtilsPlugin.PLUGIN_ID, 0, null, null);
 					try {
 						monitor.beginTask("Converting to DOT", IProgressMonitor.UNKNOWN);
 						for (Iterator<?> iterator = structuredSelection.iterator(); iterator.hasNext();) {
-							// TODO: Handle selection on multiple files for the return status (i.e. use MultiStatus when needed)
+							// TODO: Handle selection on multiple files for the
+							// return status (i.e. use MultiStatus when needed)
 							Object obj = iterator.next();
 							if (obj instanceof IFile) {
 								IFile file = (IFile) obj;
-								IFile resultFile = ResourcesPlugin.getWorkspace().getRoot().getFile(file.getFullPath().removeFileExtension().addFileExtension("dot"));
 								try {
-									HLAPIRootClass imported = PNMLUtils.importPnmlDocument(file.getLocation().toFile(), false);
-									Processor proc = MainProcessor.getProcessor(imported);
-									ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-									proc.process(imported, new PrintWriter(byteStream));
-									if (resultFile.exists()) {
-										resultFile.setContents(new ByteArrayInputStream(byteStream.toByteArray()), IResource.FORCE, monitor);
-									} else {
-										resultFile.create(new ByteArrayInputStream(byteStream.toByteArray()), IResource.FORCE, monitor);
-									}
-									ModelRepository.getInstance().destroyCurrentWorkspace();
-								} catch (InvalidIDException | ImportException | UnHandledType | CoreException | VoidRepositoryException e) {
-									return new Status(IStatus.ERROR, PnmlUtilsPlugin.PLUGIN_ID, e.getLocalizedMessage(), e);
+									// @formatter:off
+									Pnml2dot generator = new Pnml2dot(
+											URI.createFileURI(file.getLocation().toString()),
+											file.getLocation().removeLastSegments(1).toFile(),
+											Arrays.asList(new Object[] { file.getFullPath().addFileExtension("dot").lastSegment().toString() } ));
+									// @formatter:on
+									AcceleoPreferences.switchForceDeactivationNotifications(true);
+									generator.doGenerate(new BasicMonitor());
+									file.getParent().refreshLocal(IResource.DEPTH_INFINITE, new SubProgressMonitor(monitor, 1));
+								} catch (IOException | CoreException e) {
+									status.add(new Status(IStatus.ERROR, PnmlUtilsPlugin.PLUGIN_ID, e.getLocalizedMessage(), e));
 								}
 							}
 						}
 					} finally {
 						monitor.done();
 					}
-					return Status.OK_STATUS;
+					return status;
 				}
 			};
 			job.schedule();
